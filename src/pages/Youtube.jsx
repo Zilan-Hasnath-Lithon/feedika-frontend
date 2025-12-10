0ed82cf6b6c203482abe3ac011e5101f82e98604
Zilan Hasnath Lithon <zilanhasnath@gmail.com>
2025-12-10 10:56:19 +0600

    Update Youtube.jsx

--- src/pages/Youtube.jsx
+++ src/pages/Youtube.jsx
@@ -38,7 +38,7 @@ const Youtube = () => {
     };
 
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-5 mt-24">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-5 mt-14 sm:mt-16 md:mt-20">
             {rawData.map((feed) => (
                 <div
                     key={feed._id}
